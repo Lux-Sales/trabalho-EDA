5b933741da6593ab41eb2454de87fb222ad5fe9d
Lucas Sales <sales_l@hotmail.com>
2023-01-29 15:11:31 -0300

    initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+O que precisamos?
+
+fora do codigo:
+
+1 - tratar dataset para que todos os dados tenham o mesmo tamanho, para que a gente 
+consiga buscar depois da ordenação os valores.
+2 - Calcular complexidade computacional do algoritmo
+
+
+dentro do codigo:
+
+1 - Fazer função que pega x linhas aleatórias e cria um dataset para testarmos com um
+dataset reduzido
+2 - Ordenar de forma decrescente com quick sort utilizando o campo amount do dataset.
+3 - Escrever em um arquivo esse vetor ordenado
+4 - Calcular tempo de processamento
+
+*/
+
+int main(){
+    printf("teste\n");
+    return 0;
+}
