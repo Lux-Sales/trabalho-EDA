34a8200c0f929c28bfec3e3d4c9b9a0d76ea16b3
Lucas Sales <sales_l@hotmail.com>
2023-02-07 19:03:42 -0300

    fazendo funcao que cria dataset formatado

---
+++ format_csv.py
@@ -0,0 +1,17 @@
+import csv
+
+def process_row(row):
+    # Preencher cada linha com asteriscos até 20 caracteres
+    row = [content.ljust(20, '*') for content in row]
+    return row
+
+# Abrir arquivo CSV de entrada para leitura
+with open('dataset.csv', 'r') as input_file:
+    reader = csv.reader(input_file)
+    # Abrir arquivo CSV de saída para escrita
+    with open('dataset_formatado.csv', 'w', newline='') as output_file:
+        writer = csv.writer(output_file)
+        # Processar cada linha do arquivo CSV de entrada
+        for row in reader:
+            processed_row = process_row(row)
+            writer.writerow(processed_row)
